50d62da4e3a297afabfaca0b601be76f9c6ee008
Yoda <yoda@yodacode.xyz>
2022-09-08 22:23:53 -0700

    fix env vars

--- pages/api/email/[city]/[email].js
+++ pages/api/email/[city]/[email].js
@@ -1,5 +1,5 @@
 export default function (req, res) {
-    fetch('https://hooks.slack.com/workflows/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', {
+    fetch(process.env.SLACK_WEBHOOK, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
