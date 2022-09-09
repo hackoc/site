0545f43a4b6b4b875449cacb1b1bec1b131072b3
Yoda <yoda@yodacode.xyz>
2022-09-08 22:34:10 -0700

    fix favicon

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <link rel="favicon" href="/favicon.png" />
+      <link rel="shortcut icon" href="/favicon.png" />
+      <link rel="icon" href="/favicon.png" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
