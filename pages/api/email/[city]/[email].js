225dd091cfa8a06cad3921b6403bbe91514ba493
Yoda <yoda@yodacode.xyz>
2022-09-08 22:28:49 -0700

    use req.query instead of req.params

--- pages/api/email/[city]/[email].js
+++ pages/api/email/[city]/[email].js
@@ -1,12 +1,12 @@
-export default function (req, res) {
+export default async function (req, res) {
     fetch(process.env.SLACK_WEBHOOK, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            email: req.params.email,
-            ip_city: req.params.city
+            email: req.query.email,
+            ip_city: req.query.city
         })
     })
 }
