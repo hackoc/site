bd471139d1a8324d2a7909df2a188311156e1864
Yoda <yoda@yodacode.xyz>
2022-09-10 14:20:18 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -7,27 +7,32 @@ const nextConfig = {
       {
         source: '/discord',
         destination: 'https://discord.gg/cPfHW8SQ5B',
-        basePath: false
+        basePath: false,
+        permanent: false
       },
       {
         source: '/finances',
         destination: 'https://bank.hackclub.com/hackoc',
-        basePath: false
+        basePath: false,
+        permanent: false
       },
       {
         source: '/github',
         destination: 'https://github.com/hackoc',
-        basePath: false
+        basePath: false,
+        permanent: false
       },
       {
         source: '/donate',
         destination: 'https://bank.hackclub.com/donations/start/hackoc',
-        basePath: false
+        basePath: false,
+        permanent: false
       },
       {
         source: '/register',
         destination: 'https://hackoc.org',
-        basePath: false
+        basePath: false,
+        permanent: false
       }
     ]
   }
