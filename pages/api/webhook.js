df05430da58e7e8cd104d467b7cb3af570810cca
Yoda <yoda@yodacode.xyz>
2022-09-25 02:07:25 -0700

    use mail2 server

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,4 +1,4 @@
 export default async function (req, res) {
-    const response = await fetch('http://mail.hackoc.org/v1/unauthed/subscribe/webhook').then(response => response.text());
+    const response = await fetch('http://mail2.hackoc.org/v1/webhook').then(response => response.text());
     res.send(response);
 }
