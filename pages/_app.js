f2310ed67a10dda91e7fd148bda53a3b09df22ea
Yoda <yoda@yodacode.xyz>
2022-10-17 22:07:35 -0700

    Add plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +12,11 @@ function MyApp({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
+        <Script 
+          defer 
+          data-domain="hackoc.org" 
+          src="https://plausible.io/js/plausible.js"
+        />
       </Head>
       <Component {...pageProps} />
     </>
