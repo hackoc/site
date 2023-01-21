23fe89ef5fb02cce36a630472276778093bd7e35
Yoda <yoda@yodacode.xyz>
2023-01-21 14:20:08 -0800

    Fix donor spotlight

--- pages/api/donor.js
+++ pages/api/donor.js
@@ -1,5 +1,5 @@
 export default async function (req, res) {
     const response = await fetch('https://bank.hackclub.com/api/v3/organizations/hackoc/donations');
     const json = await response.json();
-    res.send(json.filter(a => !a?.transaction?.tags?.filter(tag => tag.id == 49)?.[0]).sort(() => Math.random() - 0.5)[0]?.donor?.name);
+    res.send(json.filter(a => !a?.transaction?.tags?.filter(tag => tag.id == 'tag_oWZTg3')?.[0]).sort(() => Math.random() - 0.5)[0]?.donor?.name);
 }
