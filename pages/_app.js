6a461a63dc5acd7ef4905309d9499088fd0102de
Yoda <yoda@yodacode.xyz>
2023-01-21 14:15:30 -0800

    aaa

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700,Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;300;400;500;600;700&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
       <Script 
         defer 
