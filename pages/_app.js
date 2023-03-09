1750efc365f7e6a74e6462830e4258b95e7a9f09
Yoda <yoda@yodacode.xyz>
2023-03-08 22:03:42 -0800

    Add reddit pixel

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* dangerous innerhtml script */}
+        <script dangerouslySetInnerHTML={{ __html: `
+      !function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','t2_sl22gnht', {"optOut":false,"useDecimalCurrencyValues":true,"aaid":"<AAID-HERE>","email":"<EMAIL-HERE>","externalId":"<EXTERNAL-ID-HERE>","idfa":"<IDFA-HERE>"});rdt('track', 'PageVisit');
+          `}} />
+          
         <link rel="favicon" href="/favicon.png" />
         <link rel="shortcut icon" href="/favicon.png" />
         <link rel="icon" href="/favicon.png" />
