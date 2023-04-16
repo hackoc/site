3a2cf5b6672f381b6625650b1af51adc5e50d5a2
Yoda <yoda@yodacode.xyz>
2023-04-16 11:50:26 -0700

    update

---
+++ pages/api/scrapbookpost.js
@@ -0,0 +1,39 @@
+import fetch from "node-fetch";
+
+const sleep = () => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve();
+  }, 350);
+});
+
+export function dbConnect () {
+    return new Promise((resolve, reject) => {
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    if (err) return reject(err);
+  resolve(client);
+  // perform actions on the collection object
+});
+    });
+
+}
+
+
+export default async function handler(req, res) {
+    if (req.body.token !== process.env.PRIVATE_TOKEN) return res.send("oops");
+    const dbPromise = dbConnect();
+    
+        const collection = client.db("primary").collection("scrapbook");
+        
+        const response = (await collection.insertOne({
+            name: req.body.name,
+            avatar: req.body.avatar,
+            message: req.body.message,
+            image: req.body.image,
+            
+        }));
+        res.json(response);
+        client.close();
+}
