46a21b8e3b1f598413765fa844829482ed4fb7b1
Yoda <yoda@yodacode.xyz>
2023-04-16 17:10:50 -0700

    a

---
+++ pages/api/scrapbookdelete.js
@@ -0,0 +1,41 @@
+import ShortUniqueId from "short-unique-id";
+import fetch from "node-fetch";
+
+const sleep = () => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve();
+  }, 350);
+});
+
+export function dbConnect () {
+    return new Promise((resolve, reject) => {
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    if (err) return reject(err);
+  resolve(client);
+  // perform actions on the collection object
+});
+    });
+
+}
+
+
+export default async function handler(req, res) {
+    if (req.body.token !== process.env.PRIVATE_TOKEN) return res.send("oops");
+    const client = await dbConnect();
+
+
+    
+        const collection = client.db("primary").collection("scrapbook");
+        
+        const response = (await collection.deleteOne({
+            id: req.body.id
+            
+        }));
+        res.json({
+            ok: true
+        });
+        client.close();
+}
