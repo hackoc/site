2e7c7508dfe0851cccda4e3dc45822ddf0c6445a
Yoda <yoda@yodacode.xyz>
2023-04-24 10:21:13 -0700

    don

--- pages/api/donor.js
+++ pages/api/donor.js
@@ -2,11 +2,7 @@ export default async function (req, res) {
     const response = await fetch('https://bank.hackclub.com/api/v3/organizations/hackoc/donations');
     const json = await response.json();
     res.send(
-    	json.filter(a => {
-    		const tags = a.transaction.tags.map(tag => tag.id);
-
-    		if (tags.includes('tag_MGYTLx') && !tags.includes('tag_oWZTg3')) return a;
-    	})
+    	json.filter(a =>a?.memo?.includes?.("Donation from"))
     	.sort(() => Math.random() - 0.5)[0]?.donor?.name
     );
 }
