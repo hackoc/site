500fb79d9aa9843a1db78572d6ae77442ef88c02
Yoda <yoda@yodacode.xyz>
2023-05-05 22:53:12 -0700

    yeah

---
+++ pages/schedule.js
@@ -0,0 +1,11 @@
+export default function Schedule () {
+    return (
+        <iframe src="https://schedule.hackoc.org" style={{
+            width: '100%',
+            height: '100vh',
+            border: 'none',
+            margin: '0px',
+            padding: '0px'
+        }} />
+    )
+}
