541270408cc08c56f32bd67bf1037ba290f1cb30
yodalightsabr <yoda@yodacode.xyz>
2023-08-17 21:41:35 -0700

    Update useProtection.js

--- utils/useProtection.js
+++ utils/useProtection.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 
-// view https://www.youtube.com/watch?v=pZwvrxVavnQ for additional context, consent before exiting is important – my esteemed colleague mr. ned p.
 export function useProtection (showWarning = true) {
    return useEffect(() => {
         window.onbeforeunload = () => showWarning;
