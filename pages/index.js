4a14e6184b357abf0a75b245de2d38a473df26dd
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2025-05-22 11:13:06 -0700

    Changes

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ import useMedia, { useViewport } from '../utils/useMedia';
 import Sidebar from '../components/Sidebar';
 import { FilloutStandardEmbed } from "@fillout/react";
 
-const meta_desc = "Orange County's first high school coding event since the pandemic. Join us for 12 hours of hacking, workshops, & friendship.";
+const meta_desc = "Orange County's high school hackathon";
 const theme_color = '#FA7B33';
-const social_image = '/social.png';
+const social_image = 'https://hc-cdn.hel1.your-objectstorage.com/s/v3/777fe734e848f3b800c7ea600764edf9ddd5c2ac_open-graph.png';
 
 const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
