caf7c8d4ed9607c58774f5c354be209146942dbe
yodalightsabr <yoda@yodacode.xyz>
2026-05-11 15:59:00 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import { FilloutStandardEmbed } from "@fillout/react";
 
 const meta_desc = "Orange County's high school hackathon";
 const theme_color = '#FA7B33';
-const social_image = 'https://cdn.hackclub.com/rescue?url=https://hc-cdn.hel1.your-objectstorage.com/s/v3/777fe734e848f3b800c7ea600764edf9ddd5c2ac_open-graph.png';
+const social_image = 'https://cdn.hackclub.com/019e1943-7f60-73b3-b84d-29140d089a0c/open-graph__1_.png';
 
 const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
